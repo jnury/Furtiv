64fa5734c6247efe27b39efee2fff456f93c937a
Julien Nury <julien@nury.ch>
2022-07-11 14:43:38 +0200

    Initial code commit

---
+++ Furtiv/FurtivAppContext.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Furtiv
+{
+    internal class FurtivAppContext : ApplicationContext
+    {
+        //Component declarations
+        private NotifyIcon TrayIcon;
+        private ContextMenuStrip TrayIconContextMenu;
+        private ToolStripMenuItem QuitMenuItem;
+        private ToolStripMenuItem LogsMenuItem;
+
+        public FurtivAppContext()
+        {
+            Application.ApplicationExit += new EventHandler(this.OnApplicationExit);
+
+            InitializeComponent();
+
+            RunConsoleApp();
+        }
+
+        private void InitializeComponent()
+        {
+            TrayIcon = new NotifyIcon();
+
+            TrayIcon.BalloonTipIcon = ToolTipIcon.Info;
+            TrayIcon.Text = "Furtiv Console Wrapper";
+
+            TrayIcon.Icon = Properties.Resources.PowerShellIcon;
+
+            TrayIconContextMenu = new ContextMenuStrip();
+            QuitMenuItem = new ToolStripMenuItem();
+            LogsMenuItem = new ToolStripMenuItem();
+            TrayIconContextMenu.SuspendLayout();
+
+            // 
+            // Tray Icon Context Menu
+            // 
+            this.TrayIconContextMenu.Items.AddRange(new ToolStripItem[] { this.LogsMenuItem });
+            this.TrayIconContextMenu.Items.AddRange(new ToolStripItem[] { this.QuitMenuItem });
+            this.TrayIconContextMenu.Name = "TrayIconContextMenu";
+            this.TrayIconContextMenu.Size = new Size(101, 70);
+
+            // 
+            // View Logs Menu Item
+            // 
+            this.LogsMenuItem.Name = "LogsMenuItem";
+            this.LogsMenuItem.Size = new Size(100, 22);
+            this.LogsMenuItem.Text = "View logs";
+            this.LogsMenuItem.Click += new EventHandler(this.CloseMenuItem_Click);
+
+            // 
+            // Quit Menu Item
+            // 
+            this.QuitMenuItem.Name = "QuitMenuItem";
+            this.QuitMenuItem.Size = new Size(100, 22);
+            this.QuitMenuItem.Text = "Quit";
+            this.QuitMenuItem.Click += new EventHandler(this.CloseMenuItem_Click);
+
+            TrayIconContextMenu.ResumeLayout(false);
+
+            TrayIcon.ContextMenuStrip = TrayIconContextMenu;
+
+            TrayIcon.Visible = true;
+        }
+
+        private void OnApplicationExit(object sender, EventArgs e)
+        {
+            //Cleanup so that the icon will be removed when the application is closed
+            TrayIcon.Visible = false;
+        }
+
+        private void CloseMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    
+        private void RunConsoleApp()
+        {
+            Process process = new Process();
+            // Configure the process using the StartInfo properties.
+            process.StartInfo.FileName = "powershell.exe";
+            process.StartInfo.Arguments = "-ExecutionPolicy Unrestricted -NoLogo -WindowStyle Hidden -Command C:\\Temp\\Test.ps1";
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.Start();
+            process.WaitForExit();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Furtiv
-Launch Windows command line tool (like PowerShell) without any window
+
+Furtiv is a console application wrapper for Windows. It's intended to launch command line tools (like PowerShell scripts) without displaying a window. Usefull for startup scripts and triggered user actions.
+
+This app is based on the [Doing a NotifyIcon Program the Right Way](https://www.codeproject.com/Tips/627796/Doing-a-NotifyIcon-Program-the-Right-Way) Code Project article by [Johnny J.](https://www.codeproject.com/script/Membership/View.aspx?mid=926948).
+
+Everything is a work in progress ;-)
+
+## How to code
+
+This application is built with Visual Studio Community with C# WinForm features.
